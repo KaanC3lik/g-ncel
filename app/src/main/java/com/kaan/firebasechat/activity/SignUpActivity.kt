4b174d51cf02f32663477e40063e9bb671961780
KaanC3lik <100537211+KaanC3lik@users.noreply.github.com>
2023-01-03 18:23:27 +0300

    güncel
    aa

---
+++ app/src/main/java/com/kaan/firebasechat/activity/SignUpActivity.kt
@@ -0,0 +1,108 @@
+package com.kaan.firebasechat.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.kaan.firebasechat.AKRANIM.MainActivity
+import com.kaan.firebasechat.R
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import kotlinx.android.synthetic.main.activity_sign_up.btnLogin
+import kotlinx.android.synthetic.main.activity_sign_up.btnSignUp
+import kotlinx.android.synthetic.main.activity_sign_up.etEmail
+import kotlinx.android.synthetic.main.activity_sign_up.etPassword
+
+private lateinit var auth:FirebaseAuth
+private lateinit var databaseReference: DatabaseReference
+
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        auth = FirebaseAuth.getInstance()
+        btnSignUp.setOnClickListener {
+            val userName = etName.text.toString()
+            val schoolId = schoolID.text.toString()
+            val department = department.text.toString()
+            val section = section.text.toString()
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+            val confirmPassword = etConfirmPassword.text.toString()
+
+            if (TextUtils.isEmpty(userName)) {
+                Toast.makeText(applicationContext, "username is required", Toast.LENGTH_SHORT).show()
+            }
+            if (TextUtils.isEmpty(schoolId)) {
+                Toast.makeText(applicationContext, "schoolId is required", Toast.LENGTH_SHORT).show()
+            }
+            if (TextUtils.isEmpty(department)) {
+                Toast.makeText(applicationContext, "department is required", Toast.LENGTH_SHORT).show()
+            }
+            if (TextUtils.isEmpty(section)) {
+                Toast.makeText(applicationContext, "section is required", Toast.LENGTH_SHORT).show()
+            }
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(applicationContext, "email is required", Toast.LENGTH_SHORT).show()
+            }
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(applicationContext, "password is required", Toast.LENGTH_SHORT).show()
+            }
+            if (TextUtils.isEmpty(confirmPassword)) {
+                Toast.makeText(applicationContext, "confirmPassword is required", Toast.LENGTH_SHORT).show()
+            }
+            if (password.equals(confirmPassword).not()) {
+                Toast.makeText(applicationContext, "confirmPassword is not match", Toast.LENGTH_SHORT).show()
+            }
+            registerUser(userName, email, password, schoolId, department, section)
+        }
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+
+    private fun registerUser(userName:String, email:String, password:String, schoolId:String, department:String, section:String,) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    val user: FirebaseUser? = auth.currentUser
+                    val userId:String = user!!.uid
+
+                    databaseReference = FirebaseDatabase.getInstance().getReference( "Users").child(userId)
+
+                    val hashMap:HashMap<String,String> = HashMap()
+                    hashMap.put("userId", userId)
+                    hashMap.put("username", userName)
+                    hashMap.put("schoolId", schoolId)
+                    hashMap.put("department", department)
+                    hashMap.put("section", section)
+                    hashMap.put("progileImage", "")
+
+                    databaseReference.setValue(hashMap).addOnCompleteListener(this){
+                        if (it.isSuccessful) {
+                            //open home activity
+                            etName.setText("")
+                            etEmail.setText("")
+                            etPassword.setText("")
+                            etConfirmPassword.setText("")
+                            val intent = Intent(this@SignUpActivity, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                    }
+
+                }
+            }
+    }
+}
+
